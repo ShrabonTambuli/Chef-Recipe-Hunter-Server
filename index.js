5a2fe03d451389d9bd2eeaf655ede30401db894b
ShrabonTambuli <shrabonsa@gmail.com>
2023-05-02 20:46:01 +0600

    setup ches json in index file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const chefs = require('./chef-section.json');
 const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
 })
 
 app.listen(port, () => {
