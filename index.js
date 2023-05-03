b75abc96c10793c6a74eb10eff15f6cd0592b83a
ShrabonTambuli <shrabonsa@gmail.com>
2023-05-03 16:17:00 +0600

    create single data

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ const port = 5000
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/chefs', (req, res) => {
-  res.send(chefs);
+    res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = chefs.find(chef => chef.id === id) || {};
+    res.send(chef);
 })
 
 app.listen(port, () => {
-  console.log(`Runnig port: ${port}`)
+    console.log(`Runnig port: ${port}`)
 })
